3137c697c929c0d61d40ef17eefc43aaaa58fb18
chahuigwang <chahuigwang@gmail.com>
2023-01-16 01:37:47 +0900

    Indexing data frame

--- Pandas/readExcel.py
+++ Pandas/readExcel.py
@@ -1,4 +1,12 @@
 import pandas as pd
 
 xl_file = "score.xlsx"
-df = pd.read_excel(xl_file) # 엑셀 파일 -> data frame
+df = pd.read_excel(xl_file)  # 엑셀 파일 -> data frame
+
+
+def over20():
+    result = df.loc[(df['midterm'] >= 20) & (df['final'] >= 20), ['sno', 'midterm', 'final']]
+    print(result)
+
+
+over20()
